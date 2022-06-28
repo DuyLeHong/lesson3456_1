d40126901cc543d199a753af5d613f72d36a8962
Duy Le <lehongduy.bk@gmail.com>
2022-06-28 21:34:28 +0700

    complete lesson3_screen

--- lib/lesson3_screen.dart
+++ lib/lesson3_screen.dart
@@ -5,32 +5,75 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'My app', // used by the OS task switcher
-      home: buildHomeScreen(),
+      home: MyHomeScreen(),
     ),
   );
 }
 
-Widget buildHomeScreen() {
-  return Scaffold(
-    appBar: AppBar(
-      title: Text('My Test Home Page'),
-    ),
-    body: Container(
-        alignment: Alignment.center,
-        width: 390,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          //crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text('Home Screen'),
-            SizedBox(
-              height: 20,
-            ),
-            Image.network(
-              'https://images.sudouest.fr/2020/09/13/5f5e4a9b66a4bd4d1cd490cf/widescreen/1000x500/passe-par-le-servette.jpg',
-              width: 250,
-            ),
-          ],
-        )),
+class MyHomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Test Home Page'),
+      ),
+      body: Container(
+          alignment: Alignment.center,
+          width: 390,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            //crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Home Screen',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              buildRowLayout(),
+              SizedBox(
+                height: 20,
+              ),
+              Image.network(
+                'https://images.sudouest.fr/2020/09/13/5f5e4a9b66a4bd4d1cd490cf/widescreen/1000x500/passe-par-le-servette.jpg',
+                width: 250,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Nut da duoc bam!')));
+                  },
+                  child: Text('Click me!')),
+            ],
+          )),
+    );
+  }
+}
+
+Widget buildRowLayout() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Container(
+        color: Colors.blue,
+        height: 100,
+        width: 100,
+      ),
+      Container(
+        color: Colors.green,
+        height: 100,
+        width: 100,
+      ),
+      Container(
+        color: Colors.orange,
+        height: 100,
+        width: 100,
+      )
+    ],
   );
 }
