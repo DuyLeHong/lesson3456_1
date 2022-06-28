e24fc108baa5f8e08e4c2440bd7e5f768713e4e8
Duy Le <lehongduy.bk@gmail.com>
2022-06-28 19:29:33 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/lesson3456_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lesson3456_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/testdart.dart
@@ -0,0 +1,22 @@
+
+
+void main() {
+  double a = 10, b = -50, c = 11, d = 6, e = 8;
+
+  List<double> numbers = [a, b, c, d, e];
+
+  if (numbers.isNotEmpty) {
+    numbers.sort((a, b) => b.compareTo(a)); // sap xep tu lon den be - nguoc chieu
+
+    double max = numbers.first;
+
+    print (max);
+  }
+
+
+  // double max = a;
+  // if (max < b) max = b;
+  // if (max < c) max = c;
+  // print(max);
+}
+
