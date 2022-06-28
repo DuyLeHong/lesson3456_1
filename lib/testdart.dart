407b71a4b92a8aa943bd2a51f9f9395667001e7a
Duy Le <lehongduy.bk@gmail.com>
2022-06-28 19:39:27 +0700

    update list

--- lib/testdart.dart
+++ lib/testdart.dart
@@ -1,9 +1,9 @@
 
 
 void main() {
-  double a = 10, b = -50, c = 11, d = 6, e = 8;
+  double a = 10, b = -50, c = 11, d = 6, e = 8, f = -4, g = 15;
 
-  List<double> numbers = [a, b, c, d, e];
+  List<double> numbers = [a, b, c, d, e, f, g];
 
   if (numbers.isNotEmpty) {
     numbers.sort((a, b) => b.compareTo(a)); // sap xep tu lon den be - nguoc chieu
