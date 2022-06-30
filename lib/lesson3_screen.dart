340057619e0b52916aeda67bf713e26e6d5312e4
Duy Le <lehongduy.bk@gmail.com>
2022-06-30 19:53:48 +0700

    complete lesson3_screen

--- lib/lesson3_screen.dart
+++ lib/lesson3_screen.dart
@@ -18,39 +18,55 @@ class MyHomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('My Test Home Page'),
       ),
-      body: Container(
-          alignment: Alignment.center,
-          width: 390,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            //crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                'Home Screen',
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              buildRowLayout(),
-              SizedBox(
-                height: 20,
-              ),
-              Image.network(
-                'https://codefresher.vn/wp-content/uploads/2022/01/banner_2022_flutter.jpg',
-                width: 250,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text('Nut da duoc bam!')));
-                  },
-                  child: Text('Click me!')),
-            ],
-          )),
+      body: SafeArea(
+        child: Container(
+            alignment: Alignment.center,
+            width: 390,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              //crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Home Screen',
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.w900),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                buildRowLayout(),
+                SizedBox(
+                  height: 20,
+                ),
+                Image.network(
+                  'https://codefresher.vn/wp-content/uploads/2022/01/banner_2022_flutter.jpg',
+                  width: 120,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Image.asset(
+                  'assets/Banner-QC-ChaoHe2022-1024x1024.png',
+                  width: 120,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Image.asset(
+                  'assets/images/banner_2022_android.jpg',
+                  width: 120,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text('Nut da duoc bam!')));
+                    },
+                    child: Text('Click me!')),
+              ],
+            )),
+      ),
     );
   }
 }
