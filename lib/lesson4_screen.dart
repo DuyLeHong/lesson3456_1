aa3a47dedaf88cdd57fff97b9c79b5efdfa056af
Duy Le <lehongduy.bk@gmail.com>
2022-06-30 21:18:45 +0700

    add Stack

--- lib/lesson4_screen.dart
+++ lib/lesson4_screen.dart
@@ -26,15 +26,14 @@ class MyStatelessWidget extends StatelessWidget {
         title: const Text('Expanded Column Sample'),
       ),
       body: SafeArea(
-        child: Padding( 
+        child: Padding(
           padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
           child: Center(
             child: Column(
               children: <Widget>[
-                Container(
-                  color: Colors.blue,
-                  height: 100,
-                  width: 100,
+                buildStack(),
+                SizedBox(
+                  height: 10,
                 ),
                 Expanded(
                   flex: 3,
@@ -55,9 +54,15 @@ class MyStatelessWidget extends StatelessWidget {
                           width: 100,
                         ),
                       ),
+                      SizedBox(
+                        width: 10,
+                      ),
                       Container(
                         color: Colors.grey,
                         width: 60,
+                      ),
+                      SizedBox(
+                        width: 10,
                       ),
                       Flexible(
                         child: Container(
@@ -75,16 +80,76 @@ class MyStatelessWidget extends StatelessWidget {
                     height: 300,
                   ),
                 ),
-                Container(
-                  color: Colors.blue,
-                  height: 100,
-                  width: 100,
-                ),
+                buildDecorationText(),
               ],
             ),
           ),
         ),
       ),
+    );
+  }
+
+  Widget buildDecorationText () {
+    return SizedBox(
+      width: 250,
+      height: 250,
+      child: Stack(
+        children: <Widget>[
+          Container(
+            width: 250,
+            height: 250,
+            color: Colors.white,
+          ),
+          Container(
+            padding: const EdgeInsets.all(5.0),
+            alignment: Alignment.bottomCenter,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: <Color>[
+                  Colors.black.withAlpha(0),
+                  Colors.black12,
+                  Colors.black45
+                ],
+              ),
+            ),
+            child: const Text(
+              'Foreground Text',
+              style: TextStyle(color: Colors.white, fontSize: 20.0),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildStack() {
+    return Stack(
+      alignment: AlignmentDirectional.bottomCenter,
+      clipBehavior: Clip.antiAlias,
+      children: <Widget>[
+        Container(
+
+          width: 100,
+          height: 100,
+          color: Colors.red,
+        ),
+        Container(
+          width: 90,
+          height: 90,
+          color: Colors.green,
+        ),
+        Positioned(
+          left: 0,
+          top: 90,
+          child: Container(
+            width: 80,
+            height: 80,
+            color: Colors.blue,
+          ),
+        ),
+      ],
     );
   }
 }
