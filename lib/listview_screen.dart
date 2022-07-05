16dd4fa0b6bae8596bd2e2398ccfb7dc66970a91
Duy Le <lehongduy.bk@gmail.com>
2022-07-05 19:46:54 +0700

    update listview_screen

--- lib/listview_screen.dart
+++ lib/listview_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class ListViewScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -10,24 +9,36 @@ class ListViewScreen extends StatelessWidget {
         title: const Text('ListView Screen'),
       ),
       body: SafeArea(
-        child: ListView(
-          padding: const EdgeInsets.all(8),
-          children: <Widget>[
+        child: Column(
+          children: [
             Container(
-              height: 50,
-              color: Colors.amber[600],
-              child: const Center(child: Text('Entry A')),
+              height: 150,
+              child: ListView(
+                padding: const EdgeInsets.all(8),
+                children: <Widget>[
+                  Container(
+                    height: 50,
+                    color: Colors.amber[600],
+                    child: const Center(child: Text('Entry A')),
+                  ),
+                  Container(
+                    height: 50,
+                    color: Colors.amber[500],
+                    child: const Center(child: Text('Entry B')),
+                  ),
+                  Container(
+                    height: 50,
+                    color: Colors.amber[100],
+                    child: const Center(child: Text('Entry C')),
+                  ),
+                ],
+              ),
             ),
-            Container(
-              height: 50,
-              color: Colors.amber[500],
-              child: const Center(child: Text('Entry B')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.amber[100],
-              child: const Center(child: Text('Entry C')),
-            ),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Back to Screen 1')),
           ],
         ),
       ),
