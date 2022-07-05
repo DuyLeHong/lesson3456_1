00ff2a28d523778ee2c24f3cf8fc0dbafd680cf6
Duy Le <lehongduy.bk@gmail.com>
2022-07-05 20:01:54 +0700

    update listview_screen

--- lib/listview_screen.dart
+++ lib/listview_screen.dart
@@ -34,6 +34,13 @@ class ListViewScreen extends StatelessWidget {
                 ],
               ),
             ),
+            SizedBox(
+              height: 10,
+            ),
+            buidListView2(),
+            SizedBox(
+              height: 10,
+            ),
             ElevatedButton(
                 onPressed: () {
                   Navigator.pop(context);
@@ -42,6 +49,24 @@ class ListViewScreen extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+
+  Widget buidListView2() {
+    final List<String> entries = <String>['A', 'B', 'C', 'D', 'E'];
+    final List<int> colorCodes = <int>[600, 500, 400, 300, 200];
+
+    return Container(
+      height: 200,
+      child: ListView.builder(
+          itemCount: entries.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              height: 50,
+              color: Colors.green[colorCodes[index]],
+              child: Center(child: Text('Item ${entries[index]}')),
+            );
+          }),
     );
   }
 }
